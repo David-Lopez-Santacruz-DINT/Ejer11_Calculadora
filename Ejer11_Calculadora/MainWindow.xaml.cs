f80cdc5504e8c5b723a6ab2249f8c942316690f8
abc <abc@example.com>
2020-11-03 19:48:13 +0100

    Agregar archivos de proyecto.

---
+++ Ejer11_Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejer11_Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void botonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button botonPulsado = (Button)sender;
+            pantallaTextBox.Text += botonPulsado.Tag.ToString();
+        }
+    }
+}
